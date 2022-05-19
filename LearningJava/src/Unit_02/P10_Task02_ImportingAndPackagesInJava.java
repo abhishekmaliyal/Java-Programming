f56fb6428a830d601ebe4ec3f2e4512aa6f182c5
abhishekmaliyal <abhishekmaliyal123@gmail.com>
2022-05-19 21:36:32 +0530

    Unit_02 - first

---
+++ LearningJava/src/Unit_02/P10_Task02_ImportingAndPackagesInJava.java
@@ -0,0 +1,19 @@
+
+package Unit_02;
+
+import static java.lang.System.*;
+import static java.lang.Math.*;
+import Unit_01.*;
+
+public class P10_Task02_ImportingAndPackagesInJava {
+
+	public static void main(String[] args) {
+		out.println("Welcome to Package");
+		out.println(sqrt(4));
+		System.out.println(pow(2,2));
+		System.out.println(abs(6.3));
+		String.main(null);
+	}
+	 
+
+}
